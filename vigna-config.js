302a6f66995d5a3adfc71fa7dad5fc36dbb01ad2
vignahome <vignahome@vignahome.com>
2026-07-12 11:56:35 -0500

    Conectar sitio con servidor de pagos

--- vigna-config.js
+++ vigna-config.js
@@ -1,4 +1,4 @@
 // En producción, coloca aquí la dirección HTTPS del servidor de pagos.
 window.VIGNA_CONFIG = Object.freeze({
-  apiPagosUrl: ""
+  apiPagosUrl: "https://vigna-home-bath.onrender.com"
 });
