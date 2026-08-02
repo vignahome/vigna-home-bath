c17fd719bff22febaecf491e7ede6c3ecd74c11a
vignahome <vignahome@vignahome.com>
2026-08-01 22:10:35 -0500

    Actualizar versión del módulo de contratos

--- mvp-profesionales-bootstrap.js
+++ mvp-profesionales-bootstrap.js
@@ -1,2 +1,2 @@
-import "./mvp-profesionales-cloud-ui.js?v=2";
-await import("./mvp-profesionales.js?v=2");
+import "./mvp-profesionales-cloud-ui.js?v=3";
+await import("./mvp-profesionales.js?v=3");
