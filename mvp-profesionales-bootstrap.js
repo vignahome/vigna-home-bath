8cf48ce7d30d75f4733bde5ee83f11472ae4b9af
vignahome <vignahome@vignahome.com>
2026-08-01 22:33:30 -0500

    Actualizar estilos privados del contrato

--- mvp-profesionales-bootstrap.js
+++ mvp-profesionales-bootstrap.js
@@ -1,2 +1,2 @@
-import "./mvp-profesionales-cloud-ui.js?v=3";
-await import("./mvp-profesionales.js?v=3");
+import "./mvp-profesionales-cloud-ui.js?v=4";
+await import("./mvp-profesionales.js?v=4");
